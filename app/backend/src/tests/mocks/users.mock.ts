d018e9e595a2cdc42f0ee6b3d61dda928de48546
Mathews <mathewsrodrigues9@gmail.com>
2022-11-24 13:05:13 -0300

    adicionando novos testes no /login

--- app/backend/src/tests/mocks/users.mock.ts
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -8,4 +8,9 @@ const userMock: IMockLogin = {
   password: 'secret_admin',
 }
 
-export { userMock };
+const mockToken: object =
+{
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2OTI5MzU1NCwiZXhwIjoxNjY5Mzc5OTU0fQ.H-yr2L37UFctrizP1D-J8S4JPC2NrX-4oGGErQtieyw'
+}
+
+export { userMock, mockToken };
