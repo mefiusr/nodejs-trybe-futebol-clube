feed2c80435a4f7d5fc76d250b94685d0967d96e
Mathews <mathewsrodrigues9@gmail.com>
2022-11-25 10:37:02 -0300

    ajustando testes da rota login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,5 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jsonwebtoken from 'jsonwebtoken';
+import * as bcrypt from 'bcryptjs';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -7,8 +9,7 @@ import App from '../app';
 import Users from '../database/models/Users';
 
 import { Response } from 'superagent';
-import { userMock } from './mocks/users.mock';
-import LoginService from '../services/loginService';
+import { mockToken, userMock } from './mocks/users.mock';
 
 chai.use(chaiHttp);
 
@@ -18,73 +19,90 @@ const { expect } = chai;
 
 describe('Testes da seção 1', () => {
   let chaiHttpResponse: Response;
-  describe('Post na rota /login', () => {
-    // beforeEach(async () => {
-    //   sinon.stub(Users, 'findOne').resolves(userMock as Users)
-    // });
-    it.skip('Fazer login com sucesso', async () => {
-      sinon.stub(Users, 'findOne').resolves(userMock as Users);
-      sinon.stub(LoginService, 'validatePassword').resolves(true);
-      chaiHttpResponse = await chai.request(app).post('/login').send({
-        "email": "admin@admin.com",
-        "password": "secret_admin"
-      })
-      expect(chaiHttpResponse.status).to.be.equal(200);
-      (Users.findOne as sinon.SinonStub).restore();
-      (LoginService.validatePassword as sinon.SinonStub).restore();
-    });
-    it('Fazer login com senha incorreta', async () => {
-      chaiHttpResponse = await chai.request(app).post('/login').send({
-        "email": "admin@admin.com",
-        "password": "secret"
-      })
-      sinon.stub(Users, 'findOne').resolves(userMock as Users);
-      sinon.stub(LoginService, 'validatePassword').resolves(false);
 
-      expect(chaiHttpResponse.status).to.be.equal(401);
+  describe('Testando a rota /login com POST', () => {
 
-      (Users.findOne as sinon.SinonStub).restore();
-      (LoginService.validatePassword as sinon.SinonStub).restore();
-    });
-    // it('Testa se ao fazer login, um token é gerado', async () => {
-    // chaiHttpResponse = await chai.request(app).post('/login').send({
-    //   "email": "admin@admin.com",
-    //   "password": "secret_admin"
-    // })
-    // sinon.stub(LoginController, 'prototype').resolves({ status: 200, token: mockToken })
-    // console.log(chaiHttpResponse.body);
-    // expect(chaiHttpResponse.status).to.be.equal(200);
-    // expect(chaiHttpResponse.body).to.deep.equal(mockToken);
-    // });
-    it('Testa se falha ao tentar fazer login sem email', async () => {
-      chaiHttpResponse = await chai.request(app).post('/login').send({
-        "email": '',
-        "password": "secret"
-      })
-      expect(chaiHttpResponse.status).to.be.equal(400);
-      expect(chaiHttpResponse.body).to.deep.equal({
-        message: 'All fields must be filled',
+      it('Login com senha válida', async () => {
+        sinon.stub(Users, 'findOne').resolves(userMock as Users);
+        sinon.stub(bcrypt, 'compare').resolves(true);
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            "email": "admin@admin.com",
+            "password": "secret_admin"
+          });
+
+        expect(chaiHttpResponse.status).to.be.equal(200);
+        (Users.findOne as sinon.SinonStub).restore();
+        (bcrypt.compare as sinon.SinonStub).restore();
       });
-    });
-    it('Testa se falha ao tentar fazer login sem password', async () => {
-      chaiHttpResponse = await chai.request(app).post('/login').send({
-        "email": 'admin@admin.com',
-        "password": ''
-      })
-      expect(chaiHttpResponse.status).to.be.equal(400);
-      expect(chaiHttpResponse.body).to.deep.equal({
-        message: 'All fields must be filled',
+
+      it('Login com senha inválida', async () => {
+        sinon.stub(Users, 'findOne').resolves(userMock as Users);
+        sinon.stub(bcrypt, 'compare').resolves(false);
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            "email": "admin@admin.com",
+            "password": "secret"
+          });
+
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        (Users.findOne as sinon.SinonStub).restore();
+        (bcrypt.compare as sinon.SinonStub).restore();
       });
-    });
-    it('Testa se falha ao tentar fazer login com um email inválido', async () => {
-      chaiHttpResponse = await chai.request(app).post('/login').send({
-        "email": 'admin',
-        "password": 'secret_admin'
-      })
-      expect(chaiHttpResponse.status).to.be.equal(401);
-      expect(chaiHttpResponse.body).to.deep.equal({
-        message: 'Incorrect email or password',
+
+      it('Testa se falha ao tentar fazer login sem email', async () => {
+        chaiHttpResponse = await chai.request(app).post('/login').send({
+          "email": '',
+          "password": "secret"
+        })
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.deep.equal({
+          message: 'All fields must be filled',
+        });
       });
-    });
+
+      it('Testa se falha ao tentar fazer login sem password', async () => {
+        chaiHttpResponse = await chai.request(app).post('/login').send({
+          "email": 'admin@admin.com',
+          "password": ''
+        })
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.deep.equal({
+          message: 'All fields must be filled',
+        });
+      });
+
+      it('Testa se falha ao tentar fazer login (post) com um email inválido', async () => {
+        chaiHttpResponse = await chai.request(app).post('/login').send({
+          "email": 'admin',
+          "password": 'secret_admin'
+        })
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        expect(chaiHttpResponse.body).to.deep.equal({
+          message: 'Incorrect email or password',
+        });
+      });
+
   })
-})
+
+  describe('Testes na rota /login com GET', () => {
+    it('Testa se falha ao tentar fazer login (get) sem um token', async () => {
+      chaiHttpResponse = await chai.request(app).get('/login/validate').auth('token', { type: 'bearer' })
+      sinon.stub(jsonwebtoken, 'verify').resolves('mockToken');
+
+      expect(chaiHttpResponse.status).to.be.equal(401);
+
+      expect(chaiHttpResponse.body).to.deep.equal({ message: 'Invalid token' });
+      (jsonwebtoken.verify as sinon.SinonStub).restore();
+    })
+  })
+
+
+
+});
--- app/backend/src/tests/mocks/users.mock.ts
+++ app/backend/src/tests/mocks/users.mock.ts
@@ -8,9 +8,8 @@ const userMock: IMockLogin = {
   password: 'secret_admin',
 }
 
-const mockToken: object =
-{
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2OTI5MzU1NCwiZXhwIjoxNjY5Mzc5OTU0fQ.H-yr2L37UFctrizP1D-J8S4JPC2NrX-4oGGErQtieyw'
-}
+const mockToken: string =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2OTI5MzU1NCwiZXhwIjoxNjY5Mzc5OTU0fQ.H-yr2L37UFctrizP1D-J8S4JPC2NrX-4oGGErQtieyw'
+
 
 export { userMock, mockToken };
