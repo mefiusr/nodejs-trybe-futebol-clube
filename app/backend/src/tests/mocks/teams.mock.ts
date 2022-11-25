a19374994fe4593ee3fcdb3d40a692435cd4275f
Mathews <mathewsrodrigues9@gmail.com>
2022-11-25 12:56:28 -0300

    adicionando mocks do teste da rota teams

---
+++ app/backend/src/tests/mocks/teams.mock.ts
@@ -0,0 +1,18 @@
+import ITeamMock from '../../interfaces/ITeam';
+
+const teamMock: ITeamMock[] = [
+  {
+    id: 1,
+    teamName: "Vasco da Gama"
+  },
+  {
+    id: 2,
+    teamName: "Gigante da Colina"
+  },
+  {
+    id: 3,
+    teamName: "Trem bala da Colina"
+  }
+]
+
+export {teamMock};
