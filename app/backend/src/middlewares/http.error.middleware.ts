c718e00c45ce3da0f7cf6480337cc43b592e81a4
Mathews <mathewsrodrigues9@gmail.com>
2022-11-25 10:36:46 -0300

    removendo next do middleware de error

--- app/backend/src/middlewares/http.error.middleware.ts
+++ app/backend/src/middlewares/http.error.middleware.ts
@@ -1,13 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpException from '../utils/http.exception';
 
-const httpErrorMiddleware = (err: Error, _req: Request, res: Response, next: NextFunction) => {
+const httpErrorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   const { status, message } = err as HttpException;
 
   if (status) {
     return res.status(status || 500).json({ message });
   }
-  next();
 };
 
 export default httpErrorMiddleware;
