ced348cd8c80f2ba459b08c7dd2d2954a039275f
Mathews <mathewsrodrigues9@gmail.com>
2022-11-25 22:15:00 -0300

    criando rota para inserir nova partida

--- app/backend/src/routes/matches.router.ts
+++ app/backend/src/routes/matches.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import tokenValidation from '../middlewares/tokenValidation';
 
 const matchsController = new MatchController();
 const matchRouter = Router();
 
 matchRouter.get('/', (req, res) => matchsController.getMatches(req, res));
+matchRouter.post('/', tokenValidation, (req, res) => matchsController.insertNewMatches(req, res));
 
 export default matchRouter;
