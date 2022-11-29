6bebc28d2639c538780e842689f889ee16bce492
Mathews <mathewsrodrigues9@gmail.com>
2022-11-29 13:35:30 -0300

    adicionando tipagem nas funcoes

--- app/backend/src/services/TeamsService.ts
+++ app/backend/src/services/TeamsService.ts
@@ -1,16 +1,17 @@
 import { ModelStatic } from 'sequelize';
+import { ITeam } from '../interfaces/ITeam';
 import Team from '../database/models/Team';
 
 export default class TeamsService {
   constructor(private teamModel: ModelStatic<Team> = Team) {}
 
-  async getAllTeams() {
+  async getAllTeams(): Promise<ITeam[]> {
     const teams = this.teamModel.findAll();
 
     return teams;
   }
 
-  async getTeamById(id: string) {
+  async getTeamById(id: string): Promise<ITeam | null> {
     const team = await this.teamModel.findByPk(id);
 
     return team;
