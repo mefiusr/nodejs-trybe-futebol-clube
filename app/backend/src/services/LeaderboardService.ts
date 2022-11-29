514a35c352a77c56d37e1e664f49a3a961f1b2f4
Mathews <mathewsrodrigues9@gmail.com>
2022-11-29 16:10:39 -0300

    adicionando funcao getNames e getTotalGames

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,7 +1,6 @@
-// import Team from '../database/models/Team';
-// import { ITeamMatch } from '../interfaces/ITeam';
 import MatcheService from './MatchService';
 import TeamsService from './TeamsService';
+import { ITeamLeaderBoard } from '../interfaces/ITeam';
 
 export default class LeaderboardService {
   constructor(
@@ -9,32 +8,37 @@ export default class LeaderboardService {
     private teamModel = new TeamsService(),
   ) {}
 
-  // async getNames(matches: ITeamMatch[]) {
-  //   const result = matches
-  //     .map((match) => this.teamModel.getTeamById(match.teamHome.teamName));
-  //   console.log(result);
+  async getNamesHome(idTeamHome: string): Promise<string | undefined> {
+    const nameTeam = await this.teamModel.getTeamById(idTeamHome);
+    return nameTeam?.teamName;
+  }
 
-  //   return Promise.all(result);
-  // }
+  async getTotalGamesHome(idTeamHome: number): Promise<number> {
+    const data = await this.matchesModel.getMatchesFinished();
+    const totalGames = data.filter((game) => game.homeTeam === idTeamHome);
+    return totalGames.length;
+  }
 
   async getLeaderHome() {
-    const matchesFinished = await this.matchesModel.getAllMatches();
-
-    const filterHome = matchesFinished.map((match) => {
+    const matchesFinished = await this.matchesModel.getMatchesFinished();
+    const array: ITeamLeaderBoard[] = [];
+    matchesFinished.forEach(async (match) => {
       const obj = {
-        name: match.dataValues.teamHome.teamName,
+        name: await this.getNamesHome((match.homeTeam).toString()),
         totalPoints: 9,
-        totalGames: 3,
+        totalGames: await this.getTotalGamesHome(match.homeTeam),
         totalVictories: 3,
         totalDraws: 0,
         totalLosses: 0,
         goalsFavor: 9,
-        goalsOwn: 3,
-        goalsBalance: 6,
+        goalsOwn: 2,
+        goalsBalance: 4,
         efficiency: '100.00',
       };
-      return obj;
+      array.push(obj as ITeamLeaderBoard);
     });
-    return filterHome;
+    // await Promise.all(array as ITeamLeaderBoard[]);
+
+    return array;
   }
 }
