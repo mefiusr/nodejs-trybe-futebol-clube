c60bce280e466a6b6590ec2a649418b77092d936
Mathews <mathewsrodrigues9@gmail.com>
2022-11-29 16:09:58 -0300

    modificando rota leaderboard

--- app/backend/src/routes/leaderboard.router.ts
+++ app/backend/src/routes/leaderboard.router.ts
@@ -4,6 +4,6 @@ import LeaderboardController from '../controllers/LeaderboardController';
 const leaderboardController = new LeaderboardController();
 const leaderBoardRouter = Router();
 
-leaderBoardRouter.get('/home', (req, res) => leaderboardController.getMatches(req, res));
+leaderBoardRouter.get('/home', (req, res) => leaderboardController.getMatchesHome(req, res));
 
 export default leaderBoardRouter;
