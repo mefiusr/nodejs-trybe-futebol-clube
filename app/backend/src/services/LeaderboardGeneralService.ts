e1d1b56e8b561b652c67510942178c283a2e001a
Mathews <mathewsrodrigues9@gmail.com>
2022-11-30 20:46:27 -0300

    refatorando a funcao para pegar todo o leaderboard

--- app/backend/src/services/LeaderboardGeneralService.ts
+++ app/backend/src/services/LeaderboardGeneralService.ts
@@ -6,20 +6,22 @@ export default class LeaderboardGeneralService {
     private matchService = new LeaderboardService(),
   ) { }
 
-  static setScoreGeneralBoard(team: ITeamLeaderBoard[]) {
-    const totalPoints = team[0].totalPoints + team[1].totalPoints;
-    const totalGames = team[0].totalGames + team[1].totalGames;
+  static setScoreGeneralBoard(teamHome: ITeamLeaderBoard, teamAway: ITeamLeaderBoard) {
+    const totalPoints = teamHome.totalPoints + teamAway.totalPoints;
+    const totalGames = teamHome.totalGames + teamAway.totalGames;
+    const goalsFavor = teamHome.goalsFavor + teamAway.goalsFavor;
+    const goalsOwn = teamHome.goalsOwn + teamAway.goalsOwn;
 
     const obj = {
-      name: team[0].name,
+      name: teamHome.name,
       totalPoints,
       totalGames,
-      totalVictories: team[0].totalVictories + team[1].totalVictories,
-      totalDraws: team[0].totalDraws + team[1].totalDraws,
-      totalLosses: team[0].totalLosses + team[1].totalLosses,
-      goalsFavor: team[0].goalsFavor + team[1].goalsFavor,
-      goalsOwn: team[0].goalsOwn + team[1].goalsOwn,
-      goalsBalance: team[0].goalsBalance + team[1].goalsBalance,
+      totalVictories: teamHome.totalVictories + teamAway.totalVictories,
+      totalDraws: teamHome.totalDraws + teamAway.totalDraws,
+      totalLosses: teamHome.totalLosses + teamAway.totalLosses,
+      goalsFavor,
+      goalsOwn,
+      goalsBalance: goalsFavor - goalsOwn,
       efficiency: ((totalPoints / (totalGames * 3)) * 100).toFixed(2),
     };
     return obj;
@@ -28,14 +30,11 @@ export default class LeaderboardGeneralService {
   async getLeaderBoardGeneral(): Promise<ITeamLeaderBoard[]> {
     const scoreHome = await this.matchService.getLeaderBoard();
     const scoreAway = await this.matchService.getLeaderBoardAway();
-    const scoreGeneral = [...scoreHome, ...scoreAway];
 
-    const scoreGeneralBoard: ITeamLeaderBoard[] = [];
+    const scoreGeneralBoard = scoreHome.map((team) => {
+      const filtred = scoreAway.find((e) => team.name === e.name);
 
-    scoreHome.forEach((team) => {
-      const data = scoreGeneral.filter(({ name }) => team.name === name);
-
-      scoreGeneralBoard.push(LeaderboardGeneralService.setScoreGeneralBoard(data));
+      return LeaderboardGeneralService.setScoreGeneralBoard(team, filtred as ITeamLeaderBoard);
     });
 
     return LeaderboardService.sortScore(scoreGeneralBoard);
