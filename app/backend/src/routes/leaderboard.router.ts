b826dae42c90b69bc3f7e0fa197e5f5ef3244b78
Mathews <mathewsrodrigues9@gmail.com>
2022-12-01 08:29:24 -0300

    comentando rota leaderboard

--- app/backend/src/routes/leaderboard.router.ts
+++ app/backend/src/routes/leaderboard.router.ts
@@ -4,8 +4,8 @@ import LeaderboardController from '../controllers/LeaderboardController';
 const leaderboardController = new LeaderboardController();
 const leaderBoardRouter = Router();
 
-leaderBoardRouter.get('/', (req, res) => leaderboardController.getScoreGeneral(req, res));
-leaderBoardRouter.get('/home', (req, res) => leaderboardController.getScoreHome(req, res));
+// leaderBoardRouter.get('/', (req, res) => leaderboardController.getScoreGeneral(req, res));
 leaderBoardRouter.get('/away', (req, res) => leaderboardController.getScoreAway(req, res));
+leaderBoardRouter.get('/home', (req, res) => leaderboardController.getScoreHome(req, res));
 
 export default leaderBoardRouter;
